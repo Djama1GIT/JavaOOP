4646d3603bdd557ac607e5a1a5f152f718de8063
Djama1GIT <dj.ama1@mail.ru>
2022-10-09 11:43:11 +0300

    mini-fix

--- Tasks/Tasks.java
+++ Tasks/Tasks.java
@@ -71,17 +71,43 @@ class Module1 {
     }
 }
 
-public static void main(String[] args) throws NoSuchAlgorithmException {
-    log("Модуль 1");
-    log("************************************");
-    log(Module1.remainder(3, 5));
-    log(Module1.triArea(5, 4));
-    log(Module1.animals(3, 5, 2));
-    log(Module1.profitableGamble(0.8, 3, 10));
-    log(Module1.operation(25, 13, 12));
-    log(Module1.ctoa('A'));
-    log(Module1.addUpTo(10));
-    log(Module1.nextEdge(13, 25));
-    log(Module1.sumOfCubes(new double[]{10, 20, 30}));
-    log(Module1.abcmath(10, 3, 15));
+class Tasks {
+    public static void log(String str) {
+        System.out.println(str);
+    }
+
+    public static void log(double str) {
+        System.out.println(str);
+    }
+
+    public static void log(boolean str) {
+        System.out.println(str);
+    }
+
+    public static void log(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void log(String[] str) {
+        System.out.println(String.join(" ", str));
+    }
+
+    public static void log(ArrayList<String> arr) {
+        System.out.println(Arrays.toString(arr.toArray()));
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        log("Модуль 1");
+        log("************************************");
+        log(Module1.remainder(3, 5));
+        log(Module1.triArea(5, 4));
+        log(Module1.animals(3, 5, 2));
+        log(Module1.profitableGamble(0.8, 3, 10));
+        log(Module1.operation(25, 13, 12));
+        log(Module1.ctoa('A'));
+        log(Module1.addUpTo(10));
+        log(Module1.nextEdge(13, 25));
+        log(Module1.sumOfCubes(new double[]{10, 20, 30}));
+        log(Module1.abcmath(10, 3, 15));
+    }
 }
