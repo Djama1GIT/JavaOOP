87a6600fb2ec0df6b7192a5c7540efad4b2ea64c
Djama1GIT <dj.ama1@mail.ru>
2022-10-23 19:49:12 +0300

    +module2

--- Tasks/Tasks.java
+++ Tasks/Tasks.java
@@ -68,7 +68,117 @@ class Module1 {
         return a % c == 0;
     }
 }
+class Module2 {
+    public static String repeat(String str, int count) {
+        StringBuilder result = new StringBuilder();
+        int length = str.length();
 
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < count; j++) {
+                result.append(str.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static int differenceMaxMin(int[] arr) {
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+
+        for (int i : arr) {
+            if (i < min) {
+                min = i;
+            } else if (i > max) {
+                max = i;
+            }
+        }
+
+        return (int) (max - min);
+    }
+
+    public static boolean isAvgWhole(int[] arr) {
+        double sum = 0;
+
+        for (int i : arr) {
+            sum += i;
+        }
+
+        double average = sum / arr.length;
+
+        return Math.round(average) % (average) == 0;
+    }
+
+    public static int[] cumulativeSum(int[] arr) {
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = 0;
+
+            for (int j = 0; j <= i; j++) {
+                result[i] += arr[j];
+            }
+        }
+
+        return result;
+    }
+
+    public static int getDecimalPlaces(String str) {
+        String decimal = str.split("\\.")[1];
+        return decimal.length();
+    }
+
+    public static int Fibonacci(int i) {
+        int idx = i - 1;
+        int[] fibonacci = new int[i];
+
+        for (int j = 0; j <= idx; j++) {
+            if (j == 0 || j == 1) {
+                fibonacci[j] = j + 1;
+                continue;
+            }
+
+            fibonacci[j] = fibonacci[j - 1] + fibonacci[j - 2];
+        }
+        return fibonacci[idx];
+    }
+
+    public static boolean isValid(String str) {
+        return (str.length() == 5) && str.matches("\\d+");
+    }
+
+    public static boolean isStrangePair(String str1, String str2) {
+        if (str1.equals("") && str2.equals("")) return true;
+
+        return str1.charAt(0) == str2.charAt(str2.length() - 1) && str1.charAt(str1.length() - 1) == str2.charAt(0);
+    }
+
+    public static boolean isPrefix(String word, String prefix) {
+        return word.startsWith(prefix.substring(0, prefix.length() - 2));
+    }
+
+    public static boolean isSuffix(String word, String suffix) {
+        return word.endsWith(suffix.substring(1));
+    }
+
+    public static int boxSeq(int n) {
+        int result = 0;
+
+        if (n == 0) {
+            return result;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                result -= 1;
+            } else {
+                result += 3;
+            }
+        }
+
+        return result;
+    }
+}
 class Tasks {
     public static void log(String str) {
         System.out.println(str);
@@ -107,5 +217,18 @@ class Tasks {
         log(Module1.nextEdge(13, 25));
         log(Module1.sumOfCubes(new double[]{10, 20, 30}));
         log(Module1.abcmath(10, 3, 15));
+        log("Модуль 2");
+        log("************************************");
+        log(Module2.repeat("apple", 4));
+        log(Module2.differenceMaxMin(new int[]{10, 5, 4, 30, -1}));
+        log(Module2.isAvgWhole(new int[]{3, 5, 10, 14, 8}));
+        log(Module2.cumulativeSum(new int[]{3, 4, 1, 5, 7, 8}));
+        log(Module2.getDecimalPlaces("3.5477"));
+        log(Module2.Fibonacci(7));
+        log(Module2.isValid("32454"));
+        log(Module2.isStrangePair("", ""));
+        log(Module2.isPrefix("automation", "auto-"));
+        log(Module2.isSuffix("arachnophobia", "-phobia"));
+        log(Module2.boxSeq(4));
     }
 }
